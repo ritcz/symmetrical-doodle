8896a532510d09512003f54a1a791ce57837eb6f
Chris Zoellick <czoellick@jewishseniorlife.org>
2022-03-27 16:10:00 -0400

    added categories to meals

--- packages/server/data/meals.js
+++ packages/server/data/meals.js
@@ -1,96 +1,112 @@
 export const meals = [
   {
     title: "Chicken Marsala",
+    category: "Pasta",
     description:
       "Pan seared chicken breast topped with a Marsala wine and mushroom sauce, served with Parmesan herb orzo and seasonal vegetable.",
     price: 14.85,
   },
   {
     title: "Chicken Picatta",
+    category: "Pasta",
     description:
       "Egg-battered chicken breast topped with capers, artichoke hearts, mushrooms, and tomatoes, in a lemon butter sauce. Served with rice pilaf and seasonal vegetable.",
     price: 14.85,
   },
   {
     title: "Chicken French",
+    category: "Pasta",
     description:
       "Egg-battered chicken breast in a lemon butter sauce, served with a rice pilaf and seasonal vegetable.",
     price: 14.85,
   },
   {
     title: "Chicken Parmesan",
+    category: "Pasta",
     description:
       "Lightly breaded chicken breast, fried, served with marinara sauce and penne pasta. Accompanied with seasonal vegetables.",
     price: 14.85,
   },
   {
     title: "Chicken Bruschetta",
+    category: "Pasta",
     description:
       "Marinated grilled chicken breast topped with fresh Bruschetta, and finished with a balsamic reduction. Served with vegetable couscous and seasonal vegetable.",
     price: "14.85",
   },
   {
     title: "Steakhouse Braised Tenderloin Tips",
+    category: "Fan Favorites",
     description:
       "Steakhouse braised tenderloin tips accompanied with onions, peppers, tomatoes, and mushrooms. Served over Parmesan risotto.",
     price: 16.75,
   },
   {
     title: "Salmon with Pineapple Salsa",
+    category: "Fan Favorites",
     description:
       "Seared salmon with pineapple salsa, served over rice pilaf, with seasonal vegetables.",
     price: 18.0,
   },
   {
     title: "Ziti with Meatballs or Italian Sausage",
+    category: "Pasta",
     description:
       "Ziti with marinara sauce and your choice of either meatballs or Italian sausage, served with seasonal vegetables.",
     price: 13.15,
   },
   {
     title: "Stuffed Shells (Vegetarian)",
+    category: "Pasta",
     description:
       "Stuffed shells with your choice of either marinara or Florentine sauce, with seasonal vegetables.",
     price: 12.85,
   },
   {
     title: "Mushroom Bolognese (Vegetarian)",
+    category: "Pasta",
     description:
       "Mushroom Bolognese sauce over rigatoni, topped with shaved Parmesan. Served with seasonal vegetables.",
     price: 13.15,
   },
   {
     title: "Grilled Vegetable Napoleon (Vegetarian)",
+    category: "Fan Favorites",
     description:
       "Portobello mushroom stacked with marinated grilled vegetables and topped with melted mozzarella cheese, served with brown rice and seasonal vegetables.",
     price: 14.15,
   },
   {
     title: "Southwest Cobb Salad",
+    category: "Salad",
     description:
       "Grilled marinated chicken breast, chopped romaine lettuce, hardboiled egg, avocado, tomatoes, black beans, corn, scallions, cheddar cheese, and cilantro, served with chipotle ranch dressing",
     price: 16.4,
   },
   {
     title: "Chilled Grilled Lemon Chicken with Mixed Greens",
+    category: "Salad",
     description:
       "Boneless chicken breast grilled in our own special lemon marinade and chilled, served with mixed greens and house dressing.",
     price: 14.3,
   },
   {
     title: "Chilled Marinated Flank Steak with Mixed Greens",
+    category: "Salad",
     description:
       "Flank steak grilled to perfection and sliced thin, served on mixed greens with Italian balsamic dressing.",
     price: 17.4,
   },
   {
     title: "Chilled Grilled Portobello with Mixed Greens (Vegetarian)",
+    category: "Salad",
     description:
       "Chilled grilled sliced Portobello mushroom, served on mixed greens with Italian balsamic dressing.",
     price: 14.3,
   },
   {
     title: "Greek Pasta Salad",
+    category: "Salad",
     description:
       "Bow tie pasta tossed in a light Greek dressing, with fresh basil, feta cheese, olives, sun-dried tomatoes, and baby spinach. Topped with your choice of chilled grilled chicken breast, or Portobello mushroom.",
     price: 14.3,
